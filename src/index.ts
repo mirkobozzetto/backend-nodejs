cc537e7bda790abd029293f3c618d7db4be73faf
Mirko Bozzetto <mirkouch@gmail.com>
2024-01-28 19:53:42 +0100

    Add pg package for PostgreSQL integration

--- src/index.ts
+++ src/index.ts
@@ -26,3 +26,25 @@ server.listen(8080, () => {
 });
 
 const DB_URL = process.env.DATABASE_URL;
+
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    require: true,
+  },
+});
+
+async function getPostgresVersion() {
+  const client = await pool.connect();
+  try {
+    const response = await client.query("SELECT version()");
+    console.log(response.rows[0]);
+  } finally {
+    client.release();
+  }
+}
+
+getPostgresVersion();
